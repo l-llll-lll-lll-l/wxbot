4fd1a2c6deb7dbf8021b9ec6187a4be41ba01ef0
l-llll-lll-lll-l <luojiashuo6237@emails.bjut.edu.cn>
2024-11-15 23:52:53 +0800

    修改退出信号

--- wxbot.py
+++ wxbot.py
@@ -1,8 +1,8 @@
 import dashboard
 import threading
 from core.wxutils import AutoReplyBot
-import signal
 import os
+import signal
 import sys
 os.environ['MLC_LLM_HOME'] = "./"
 
@@ -12,12 +12,11 @@ def run_web():
 
 def run_bot():
     bot = AutoReplyBot(model_path="./qwen2.5_0.5b/Qwen2.5-0.5B-Instruct-q0f16-MLC", db_path="bot.db")
-    print("Bot is running...")
+    signal.signal(signal.SIGINT, bot.signal_handler)
+    print("Bot is running...(use Ctrl+C to stop the bot)")
     bot.auto_reply()
 
-def signal_handler(sig, frame):
-    sys.exit(0)
-    
+print("Starting bot and web dashboard...")
 threading.Thread(target=run_web).start()
 run_bot()
-signal.signal(signal.SIGINT, signal_handler)
+print("close the window to exit")
