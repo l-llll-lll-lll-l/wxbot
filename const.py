7731188ab64d70f516466011f227ec4ea919ac05
l-llll-lll-lll-l <luojiashuo6237@emails.bjut.edu.cn>
2024-12-11 19:42:12 +0800

    修改readme

--- const.py
+++ const.py
@@ -1,1 +1,2 @@
 db_path = "bot.db"
+modelname = "qwen2.5:0.5b"
--- wxbot.py
+++ wxbot.py
@@ -2,12 +2,13 @@ import dashboard
 import threading
 from core.wxutils import AutoReplyBot
 import signal
+from const import modelname, db_path
 
 def run_web():
     dashboard.app.run()
 
 def run_bot():
-    bot = AutoReplyBot(modelname="qwen2.5:0.5b", db_path="bot.db")
+    bot = AutoReplyBot(modelname=modelname, db_path=db_path)
     signal.signal(signal.SIGINT, bot.signal_handler)
     print("Bot is running...(use Ctrl+C to stop the bot)")
     bot.auto_reply()
